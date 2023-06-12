483c4ee5362f2e5911c766ccaf09a47c430bf7d8
PabloFS <pablofs02@tutanota.com>
2023-06-12 06:08:45 +0200

    Nuevo sistema de configuración y más limpieza de código

--- src/conf.rs
+++ src/conf.rs
@@ -1,14 +1,40 @@
 use std::collections::HashMap;
+use std::env::var;
 use std::fs::read_to_string;
 
+#[must_use]
 pub fn cargar() -> HashMap<String, String> {
-    let mut d = HashMap::new();
-    let conf = read_to_string("/home/.config/disco").expect("Error al cargar configuración");
-    for rel in conf.lines() {
-        let rel: Vec<&str> = rel.split("=>").collect();
-        let base = String::from(rel[0].trim()).replace("~/", "/home/");
-        let copia = String::from(rel[1].trim()).replace("~/", "/home/");
-        d.insert(base, copia);
+    let mut directorios = HashMap::new();
+    let home = var("HOME").unwrap_or_else(|_| "/home".to_owned());
+    let mut origen = String::from(&home);
+    let mut destino = "/".to_owned();
+    let ruta_archivo_conf = format!("{home}/.config/disco");
+    let archivo_conf = read_to_string(ruta_archivo_conf).expect("Error al cargar configuración");
+    for relatio in archivo_conf.lines() {
+        let relatio = relatio.replace('~', &home[..]);
+        if relatio.starts_with('[') && relatio.ends_with(']') {
+            if relatio.starts_with("[[") && relatio.ends_with("]]") {
+                let contenido = &relatio[2..(relatio.len() - 2)];
+                destino = String::from(contenido);
+            } else {
+                let contenido = &relatio[1..(relatio.len() - 1)];
+                origen = if contenido.starts_with('/') {
+                    String::from(contenido)
+                } else {
+                    format!("{home}/{contenido}")
+                };
+            }
+        } else {
+            let relatio: Vec<&str> = relatio.split("=>").collect();
+            let dir_base = if relatio[0].starts_with('/') {
+                String::from(relatio[0].trim())
+            } else {
+                format!("{origen}/{}", relatio[0].trim())
+            };
+            let copia = String::from(relatio[1].trim());
+            let dir_copia = format!("{destino}/{copia}");
+            directorios.insert(dir_base, dir_copia);
+        }
     }
-    d
+    directorios
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,45 @@
 pub mod conf;
 use std::fs::{copy, read_dir, File};
 use std::io::{BufReader, Read};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 pub fn actualizar_directorio(dir_base: &String, dir_copia: &String) {
     let mut es_directorio: bool = false;
-    let orig = File::open(&dir_base).unwrap();
+    let orig = File::open(dir_base).unwrap();
     if orig.metadata().unwrap().is_dir() {
         es_directorio = true;
     }
-    if let Ok(dest) = File::open(&dir_copia) {
-        if es_directorio {
-            let subdirs = read_dir(&dir_base).unwrap();
-            for subdir in subdirs {
-                let subdir = subdir.unwrap();
-                let carpeta = subdir.file_name().to_str().unwrap().to_string();
-                let ruta = subdir.path().to_str().unwrap().to_string();
-                actualizar_directorio(&ruta, &format!("{dir_copia}/{carpeta}"));
+    File::open(dir_copia).map_or_else(
+        |_| {
+            if es_directorio {
+                copiar_directorio(dir_base, dir_copia);
+            } else {
+                let ruta = PathBuf::from(dir_copia);
+                let ruta = ruta.parent().unwrap().to_str().unwrap();
+                std::fs::create_dir_all(ruta).unwrap();
+                copy(dir_base, dir_copia).unwrap_or_else(|_| panic!("{dir_base}>{dir_copia}"));
             }
-        } else if hay_cambios(orig, dest) {
-            copy(&dir_base, &dir_copia).expect(&format!("{dir_base}>{dir_copia}"));
-            println!("{dir_base} => {dir_copia}");
+        },
+        |dest| {
+            if es_directorio {
+                let subdirs = read_dir(dir_base).unwrap();
+                for subdir in subdirs {
+                    let subdir = subdir.unwrap();
+                    let carpeta = subdir.file_name().to_str().unwrap().to_string();
+                    let ruta = subdir.path().to_str().unwrap().to_string();
+                    actualizar_directorio(&ruta, &format!("{dir_copia}/{carpeta}"));
+                }
+            } else if hay_cambios(&orig, &dest) {
+                copy(dir_base, dir_copia).unwrap_or_else(|_| panic!("{dir_base}>{dir_copia}"));
+                println!("{dir_base} => {dir_copia}");
+            }
         }
-    } else {
-        if es_directorio {
-            copiar_directorio(dir_base, dir_copia);
-        } else {
-            let ruta = PathBuf::from(dir_copia);
-            let ruta = ruta.parent().unwrap().to_str().unwrap();
-            std::fs::create_dir_all(ruta).unwrap();
-            copy(&dir_base, &dir_copia).expect(&format!("{dir_base}>{dir_copia}"));
-        }
-    }
+    );
 }
 
 fn copiar_directorio(dir_base: &String, dir_copia: &String) {
-    std::fs::create_dir_all(&dir_copia).expect(dir_copia);
-    let subdirectorios = read_dir(&dir_base).expect(dir_base);
+    std::fs::create_dir_all(dir_copia).expect(dir_copia);
+    let subdirectorios = read_dir(dir_base).expect(dir_base);
     for subdir in subdirectorios {
         let ruta = subdir.unwrap().path();
         let d = File::open(&ruta).unwrap();
@@ -47,12 +50,12 @@ fn copiar_directorio(dir_base: &String, dir_copia: &String) {
             println!("{dir_orig} => {dir_dest}");
             copiar_directorio(&ruta.to_str().unwrap().to_string(), &dir_dest);
         } else {
-            copiar_archivo(ruta, &dir_copia);
+            copiar_archivo(&ruta, dir_copia);
         }
     }
 }
 
-fn copiar_archivo(dir_base: PathBuf, dir_copia: &String) {
+fn copiar_archivo(dir_base: &Path, dir_copia: &str) {
     let arch = dir_base.file_name().unwrap().to_str().unwrap();
     let ruta = dir_base.to_str().unwrap().to_string();
     let ruta_dest = format!("{dir_copia}/{arch}");
@@ -60,27 +63,21 @@ fn copiar_archivo(dir_base: PathBuf, dir_copia: &String) {
     println!("{ruta} => {ruta_dest}");
 }
 
-fn hay_cambios(orig: File, dest: File) -> bool {
+fn hay_cambios(orig: &File, dest: &File) -> bool {
     if orig.metadata().unwrap().len() != dest.metadata().unwrap().len() {
         return true;
     }
-    let mut lector_orig = BufReader::new(&orig);
-    let mut lector_dest = BufReader::new(&dest);
+    let mut lector_orig = BufReader::new(orig);
+    let mut lector_dest = BufReader::new(dest);
     let mut buf_orig = [0; 10000];
     let mut buf_dest = [0; 10000];
-    loop {
-        if let Ok(n1) = lector_orig.read(&mut buf_orig) {
-            if n1 > 0 {
-                if let Ok(n2) = lector_dest.read(&mut buf_dest) {
-                    if n1 == n2 {
-                        if buf_orig == buf_dest {
-                            continue;
-                        }
-                    }
-                    return true;
+    while let Ok(n1) = lector_orig.read(&mut buf_orig) {
+        if n1 > 0 {
+            if let Ok(n2) = lector_dest.read(&mut buf_dest) {
+                if n1 == n2 && buf_orig == buf_dest {
+                    continue;
                 }
-            } else {
-                break;
+                return true;
             }
         } else {
             break;
